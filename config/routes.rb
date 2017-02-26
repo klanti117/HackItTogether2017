455169cd6f5b0491de890cbc044f348728f25125
klanti islam <klantiislam@gmail.com>
2017-02-26 11:58:47 -0500

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/show'
+
+  get 'users/edit'
+
+  get 'users/index'
+
+  root "pages#home"
+  get 'pages/home'
+
+
+  get 'pages/about'
+  resources :users 
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
